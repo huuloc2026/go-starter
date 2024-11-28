56a01f9636a94a784a4f64ad3f0da1ab67f87892
Huu Loc <huuloc2026@gmail.com>
2024-11-28 17:50:44 +0700

    switchifelse

--- SwitchCase/main.go
+++ SwitchCase/main.go
@@ -3,5 +3,17 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello World")
+	array := []int{1, 2, 3, 4, 5}
+	CountEven(array)
+
+}
+func CountEven(array []int) {
+	var countEven int
+	for _, value := range array {
+		if value%2 == 0 {
+			countEven++
+		}
+	}
+	fmt.Println("count::", countEven)
+	fmt.Println("countOdd::", len(array)-countEven)
 }
