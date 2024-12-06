b4efc7e49d05416b3601dc9132d9ceeb1755da5a
Huu Loc <huuloc2026@gmail.com>
2024-12-07 00:05:12 +0700

    routine

---
+++ goroutine/goroutine.go
@@ -0,0 +1,28 @@
+// package goroutine
+
+// import (
+// 	"fmt"
+// 	"sync"
+// 	"time"
+// )
+
+// func goroutine() {
+// 	var wg = sync.WaitGroup{}
+// 	wg.Add(2)
+// 	go func() {
+// 		count("Sheep")
+// 		wg.Done()
+// 	}()
+// 	go func() {
+// 		count("Elephant")
+// 		wg.Done()
+// 	}()
+// 	wg.Wait()
+// 	fmt.Println("Done")
+// }
+// func count(name string) {
+// 	for i := 1; i <= 5; i++ {
+// 		fmt.Println(name, i)
+// 		time.Sleep(time.Second)
+// 	}
+// }
