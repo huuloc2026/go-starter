32c99ddbaba31d4a9afd18085d807087838dcd8f
Huu Loc <huuloc2026@gmail.com>
2024-12-07 21:47:51 +0700

    hello world

---
+++ channel/main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+)
+
+func main() {
+	var wg = sync.WaitGroup{}
+	ch := make(chan int)
+	wg.Add(2)
+	go func() {
+		i := <-ch
+		fmt.Println(i)
+		ch <- 27
+		wg.Done()
+	}()
+	go func() {
+		ch <- 42
+		fmt.Println(<-ch)
+		wg.Done()
+	}()
+	wg.Wait()
+
+}
