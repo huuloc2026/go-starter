feee18b875ca740ea22a13f579aa6ffce42577ce
Huu Loc <huuloc2026@gmail.com>
2024-12-13 16:24:29 +0700

    learn 13/12

--- goroutine/main.go
+++ goroutine/main.go
@@ -31,3 +31,32 @@ func increment() {
 	m.Unlock()
 	wg.Done()
 }
+
+// package goroutine
+
+// import (
+// 	"fmt"
+// 	"sync"
+// 	"time"
+// )
+
+// func goroutine() {
+// 	var wg = sync.WaitGroup{}
+// 	wg.Add(2)
+// 	go func() {
+// 		count("Sheep")n k 0, 0,l0
+// 		wg.Done()
+// 	}()
+// 	go func() {
+// 		count("Elephant")
+// 		wg.Done()
+// 	}()
+// 	wg.Wait()
+// 	fmt.Println("Done")
+// }
+// func count(name string) {
+// 	for i := 1; i <= 5; i++ {
+// 		fmt.Println(name, i)s
+// 		time.Sleep(time.Second)
+// 	}
+// }
