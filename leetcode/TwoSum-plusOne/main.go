8c3b1308bcdc1f8a214db9e283adfef6639bd0d2
Huu Loc <huuloc2026@gmail.com>
2024-12-16 21:14:54 +0700

    leetcode

---
+++ leetcode/TwoSum-plusOne/main.go
@@ -0,0 +1,28 @@
+package main
+
+func main() {
+	numbers := []int{2, 3, 4}
+	target := 4
+	twoSum(numbers, target)
+}
+
+func twoSum(numbers []int, target int) []int {
+	left := 0
+	right := len(numbers) - 1
+
+	for left < right {
+		sum := numbers[left] + numbers[right]
+
+		if sum == target {
+			return []int{left + 1, right + 1}
+		} else if sum < target {
+
+			left++
+		} else {
+
+			right--
+		}
+	}
+	// If no solution exists
+	return []int{-1, -1}
+}
